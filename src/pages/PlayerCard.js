0628da443f270c53f4725b1959d9b5fffb84e43b
Anthony Lofredo <anthony.lofredo@gmail.com>
2021-11-03 18:54:12 -0400

    https fix

--- src/pages/PlayerCard.js
+++ src/pages/PlayerCard.js
@@ -10,12 +10,12 @@ const PlayerCard = (props) => {
 //API call to get the active roster
     const getPlayerDetails = async () => {
         if (props.location.state.position === "P"){
-            const url = `http://lookup-service-prod.mlb.com/json/named.sport_career_pitching.bam?league_list_id='mlb'&game_type='R'&player_id='${playerID}'`
+            const url = `https://lookup-service-prod.mlb.com/json/named.sport_career_pitching.bam?league_list_id='mlb'&game_type='R'&player_id='${playerID}'`
             const response = await fetch(url)
             const data = await response.json()
             setCareerStats(data.sport_career_pitching.queryResults.row)
         } else {
-            const url = `http://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam?league_list_id='mlb'&game_type='R'&player_id='${playerID}'`
+            const url = `https://lookup-service-prod.mlb.com/json/named.sport_career_hitting.bam?league_list_id='mlb'&game_type='R'&player_id='${playerID}'`
             const response = await fetch(url)
             const data = await response.json()
             setCareerStats(data.sport_career_hitting.queryResults.row)
